5f45e31ebfbc785bfa2a123aa864c23203264867
imisstheoldpabl0 <prubiobusiness@gmail.com>
2024-05-09 11:50:16 +0200

    jest fix

--- __test__/math.test.js
+++ __test__/math.test.js
@@ -1,10 +1,10 @@
 const { sum, mul, sub, div } = require('../src/math');
 
 test('Adding 1 + 1 equals 2', () => {
-  expect(sum(1, 1)).toBe(2222222);
+  expect(sum(1, 1)).toBe(2);
 });
 test('Multiplying 1 * 1 equals 1', () => {
-  expect(mul(1, 1)).toBe(12222);
+  expect(mul(1, 1)).toBe(1);
 });
 test('Subtracting 1 - 1 equals 0', () => {
   expect(sub(1, 1)).toBe(0);
